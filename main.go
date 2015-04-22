c859698869dc763688361fc562723c2d0b569e6b
wooken <kenny@kenwoo.io>
2015-04-22 00:47:33 -0700

    prep main.go for builds

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	var filename string
 	var data []string
 	filename = GetArgs()
 	data = ParseFile(filename)
-	for i := range data {
-		fmt.Println(data[i])
-	}
+	PrintTonnage(data)
 }
