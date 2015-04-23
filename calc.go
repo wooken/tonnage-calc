b169006ae46d7c4e333d5604d8eccad248d50889
wooken <kenny@kenwoo.io>
2015-04-22 22:45:43 -0700

    calculation functions complete
    TODO:
    - refactor code for readability

--- calc.go
+++ calc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 const (
@@ -11,39 +12,93 @@ const (
 	SplitSetDataRegexp = `\s`
 )
 
-func PrintTonnage(data []string) {
-	var output []string
+type ExerciseData struct {
+	Name string
+	Data []string
+}
+
+type Exercises []ExerciseData
+
+func (e *Exercises) WriteExerciseData(data []string) {
 	for i := range data {
 		if isExerciseName(data[i]) {
-			output = append(output, fmt.Sprintf("%s", data[i]))
-		} else if isSetData(data[i]) {
-			setParts := splitSetData(data[i])
-			output = append(output, fmt.Sprintf("\t%s %s", setParts[1], setParts[2]))
+			(*e) = expandExerciseSlice(*e)
+			index := len(*e) - 1
+			(*e)[index].Name = data[i]
+			for j := i + 1; j < len(data); j++ {
+				if isExerciseName(data[j]) {
+					break
+				}
+				(*e)[index].Data = append((*e)[index].Data, fmt.Sprintf("%s", data[j]))
+			}
 		}
 	}
+}
 
-	output = append(output, "\nTonnage:")
-	for i := range data {
-		if isExerciseName(data[i]) {
-			// split string and print exercise name
+func (e *Exercises) OutputData() {
+	for i := range *e {
+		fmt.Println((*e)[i].Name)
+		for j := range (*e)[i].Data {
+			t := splitSetData((*e)[i].Data[j])
+			fmt.Printf("\t%s %s\n", t[1], t[2])
 		}
 	}
+}
 
-	for i := range output {
-		fmt.Println(output[i])
+func (e *Exercises) OutputTonnage() {
+	fmt.Println()
+	fmt.Println("Tonnage:")
+	totalTonnage := 0
+	for i := range *e {
+		name := regexp.MustCompile(`\s`).Split((*e)[i].Name, 2)
+		fmt.Printf("%s: ", name[1])
+		tonnage := 0
+		for j := range (*e)[i].Data {
+			vals := extractIntegers((*e)[i].Data[j])
+			ton := calcTonnage(vals)
+			tonnage = tonnage + ton
+		}
+		fmt.Println(tonnage)
+		totalTonnage = totalTonnage + tonnage
 	}
+	fmt.Printf("Total: %d\n", totalTonnage)
+}
+
+func calcTonnage(vals []int) int {
+	tonnage := 1
+	for i := range vals {
+		tonnage = tonnage * vals[i]
+	}
+	return tonnage
+}
+
+func extractIntegers(s string) []int {
+	// refactor this, completely unreadable
+
+	vals := make([]int, 3)
+	t := splitSetData(s)
+	t1 := regexp.MustCompile("x").Split(t[1], -1)
+	t2 := regexp.MustCompile("@").Split(t[2], -1)
+	vals[0], _ = strconv.Atoi(t1[0])
+	vals[1], _ = strconv.Atoi(t1[1])
+	vals[2], _ = strconv.Atoi(t2[1])
+	return vals
 }
 
 func isExerciseName(s string) bool {
-	re := regexp.MustCompile(ExerciseNameRegexp)
-	return re.MatchString(s)
+	return regexp.MustCompile(ExerciseNameRegexp).MatchString(s)
 }
 
 func isSetData(s string) bool {
-	re := regexp.MustCompile(SetDataRegexp)
-	return re.MatchString(s)
+	return regexp.MustCompile(SetDataRegexp).MatchString(s)
 }
 
 func splitSetData(s string) []string {
 	return regexp.MustCompile(SplitSetDataRegexp).Split(s, -1)
+}
+
+func expandExerciseSlice(e []ExerciseData) []ExerciseData {
+	t := make([]ExerciseData, len(e)+1)
+	copy(t, e)
+	return t
 }
--- calc_test.go
+++ calc_test.go
@@ -4,6 +4,35 @@ import (
 	"testing"
 )
 
+func TestWriteExerciseData(t *testing.T) {
+	var e Exercises
+	d := []string{"A. Push Press", "i. 4x5 @95", "i. 4x5 @95", "i. 4x5 @95"}
+	e.WriteExerciseData(d)
+	if len(e) != 1 {
+		t.Fail()
+	}
+	if len(e[0].Data) != 3 {
+		t.Fail()
+	}
+}
+
+func TestCalcTonnage(t *testing.T) {
+	vals := []int{5, 3, 115}
+	if calcTonnage(vals) != 1725 {
+		t.Fail()
+	}
+}
+
+func TestExtractIntegers(t *testing.T) {
+	s := "i. 5x3 @115"
+	vals := extractIntegers(s)
+	if vals[0] != 5 ||
+		vals[1] != 3 ||
+		vals[2] != 115 {
+		t.Fail()
+	}
+}
+
 func TestIsExerciseName(t *testing.T) {
 	name := "A. Push Press"
 	data := "i. 4x5 @95"
